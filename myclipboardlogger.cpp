450a5694025b6d9f0dd4f14a4506a41d2c8a7a27
Gerald Büchler <dev@genomast.de>
2019-11-17 13:48:23 +0100

    Added the qtimer attribute to class myclipboardlogger

--- myclipboardlogger.cpp
+++ myclipboardlogger.cpp
@@ -1,5 +1,19 @@
 #include "myclipboardlogger.hpp"
 
+void MyClipboardLogger::setTimer(QTimer *to)
+{
+    if(nullptr != this->timer)
+    {
+        delete  this->timer;
+    }
+    this->timer = to;
+}
+
+QTimer *MyClipboardLogger::getTimer()
+{
+    return this->timer;
+}
+
 MyClipboardLogger::MyClipboardLogger(QObject *parent) : QObject(parent)
 {
 
--- myclipboardlogger.hpp
+++ myclipboardlogger.hpp
@@ -2,10 +2,16 @@
 #define MYCLIPBOARDLOGGER_HPP
 
 #include <QObject>
+#include <QTimer>
 
 class MyClipboardLogger : public QObject
 {
     Q_OBJECT
+private:
+    QTimer *timer = nullptr;
+protected:
+    void setTimer(QTimer *to);
+    QTimer * getTimer(void);
 public:
     explicit MyClipboardLogger(QObject *parent = nullptr);
 
