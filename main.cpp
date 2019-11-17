3544e67cdd076a0191e07c87a76867595cf55762
Gerald Büchler <dev@genomast.de>
2019-11-18 00:31:47 +0100

    activate the class (myClipboardLogger) in main and started the timer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 
 #include <QApplication>
+#include "myclipboardlogger.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 //    MainWindow w;
 //    w.show();
+    MyClipboardLogger logger;
+    logger.startTimer();
+
     return a.exec();
 }
