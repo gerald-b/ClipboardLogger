777ad762bb409ebe62162b487836b56461ffa653
Gerald Büchler <dev@genomast.de>
2019-11-21 12:01:34 +0100

    Handle Console Output on Windows and Linux correct

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 
 #include <QApplication>
+#include <iostream>
+#include <cstdlib>
 #include "myclipboardlogger.hpp"
+
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-    MyClipboardLogger logger;
-    logger.startTimer();
+	if (argc > 1)
+	{
+		QCoreApplication a(argc, argv);
+		std::cout << MY_VERSION << std::endl;
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+#if defined( Q_OS_WIN )
+		::ShowWindow(::GetConsoleWindow(), SW_HIDE); //hide console window
+#endif
+		QApplication a(argc, argv);
+		MyClipboardLogger *logger = new MyClipboardLogger();
+		logger->startTimer();
+		int e = a.exec();
+		delete logger; //needed to execute deconstructor
+		exit(e); //needed to exit the hidden console
+	}
 
-    return a.exec();
+	return EXIT_FAILURE;
 }
